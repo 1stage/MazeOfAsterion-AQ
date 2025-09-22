d07d4892594aa47418e9cb6e942bae25599d0538
Sean P. Harrington <sph@1stage.com>
2025-09-21 21:15:26 -0700

    Last Commit before branching and reorganizing code

--- src/asterion.asm
+++ src/asterion.asm
@@ -3,7 +3,7 @@ INCLUDE "asterion.inc"
 
 org $c000
 
-; include "asterion_low_rom.asm"
+include "asterion_low_rom.asm"
 
 TITLE_SCREEN:
 INCBIN "asterion_title.scr"
@@ -11,8 +11,6 @@ INCBIN "asterion_title.scr"
 TS_EYES_OPEN_CHR EQU TITLE_SCREEN + 726
 TITLE_SCREEN_COL EQU TITLE_SCREEN + 1024
 TS_EYES_OPTN_COL EQU TITLE_SCREEN + 1750
-
-include "asterion_low_rom.asm"
 
 include "scramblecode.asm"
 
